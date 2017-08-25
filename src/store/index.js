d8122f473b33633e8d9aa75af187db9413d99f81
lukasznowak85 <lukasznowak85@gmail.com>
2017-08-25 16:04:18 +0200

    apply loading middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,15 @@
-import {createStore} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import rootReducer from './rootReducer';
+import logger from './middlewares/logger';
 
-const store = createStore(rootReducer, window.devToolsExtension && window.devToolsExtension());
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(
+    applyMiddleware(logger)
+  )
+);
+// const store = createStore(rootReducer, window.devToolsExtension && window.devToolsExtension());
 
 export default store;
