272324c9c1582eba762273f340ccf5b2f6037d63
lukasznowak85 <lukasznowak85@gmail.com>
2017-08-25 10:20:32 +0200

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        elo
+      </div>
+    )
+  }
+}
+
+export default App;
