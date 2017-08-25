04cf1fabc7eed029b595837703149dece8827848
lukasznowak85 <lukasznowak85@gmail.com>
2017-08-25 12:32:45 +0200

    added fetching details

--- src/views/Details.js
+++ src/views/Details.js
@@ -1,16 +1,45 @@
 import React, {Component} from 'react';
+import {customFetch} from '../services/fetch';
 import {Link} from 'react-router-dom';
 
-const Details = props => {
-  const to = "/main";
+class Details extends Component  {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div className="details">
-      Details
-      <br/>
-      <Link to={to}>Go back to main</Link>
-    </div>
-  )
+    this.state = {
+      post: {}
+    }
+  }
+
+  componentDidMount() {
+    console.log('this.state', this.props.match.params.id);
+    const postId = this.props.match.params.id
+    this.setState({loading: true})
+
+    customFetch('https://jsonplaceholder.typicode.com/posts/' + postId)
+      .then((res) => res.json())
+      .then((postData) => {
+        this.setState({post: postData})
+        this.setState({loading: false})
+      })
+  }
+  
+  render() {
+    const {id, title, body} = this.state.post;
+    const {loading} = this.state;
+    const to = "/main";
+    return (
+      <div className="details">
+      {loading ? 'Loading' : 
+        <div>
+          <h3>{id}: {title}</h3>
+          <p>{body}</p>
+          <br/>
+          <Link to={to}>Go back to main</Link>
+        </div>}
+      </div>
+    )
+  }
 }
 
 export default Details;
